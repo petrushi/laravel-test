7a65b3228ac40a058443d3e3a55a9f9fe4ee72b5
stanislaw8 <pavlovinthesky@gmail.com>
2021-09-03 03:53:04 +0300

    add user_id to requests

--- laravel-form/routes/web.php
+++ laravel-form/routes/web.php
@@ -17,11 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/requests', [RequisitionController::class, 'index']);
-Auth::routes();
+Route::get('/requests', [RequisitionController::class, 'index'])->middleware('auth');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Auth::routes();
 
-Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
